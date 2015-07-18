f5a8424b6dec487b1d5680f0e69692098457c0b6
Leonel Maldonado <lmaldonado@4geeks.co>
2015-07-18 08:47:21 -0400

    fix fonts, broken links

--- files/core.js
+++ files/core.js
@@ -3,7 +3,7 @@
         defaults: {
             reloadbox: " ",
             loadBox: "", 
-            loadErrorMessage: "THE PAGE YOU WERE LOOKING FOR COULD NOT BE FOUND.",
+            loadErrorMessage: "NOT FOUND.",
             loadErrorBacklinkText: "Back to the last page",
             bouncingBoxes: "",
             topToBottom: false,
@@ -488,3 +488,12 @@
         else a.error("Method " + b + " does not exist on jQuery.coretemp")
     }
 }(jQuery);
+ 
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-65269371-1', 'auto');
+  ga('send', 'pageview');
+ 
